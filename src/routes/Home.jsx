40774278b322e5b05f2f196bd8bc8f50d373fd5a
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-11-05 13:50:52 -0500

    add quick nav for demo build

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <p>Hello World</p>
+      <Link to='/'>home</Link>
+      <br/>
+       <Link to='/about'>about</Link>
+       <br/>
+       <Link to='/sign-up'>sign-up</Link>
     </div>
   )
 }
--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
     <div>
+      
       <p>Hello World</p>
+      <Link to='/'>home</Link>
+      <br/>
+       <Link to='/About'>about</Link>
+       <br/>
+       <Link to='/SignUp'>sign-up</Link>
     </div>
   )
 }
