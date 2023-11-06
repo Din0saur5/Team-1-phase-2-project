7359556070f76adde31141a1b604841d13ba7461
Matthew Tran <matt.h.tran@gmail.com>
2023-11-06 08:52:13 -0500

    fixed navbar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
-function ColorSchemesExample() {
+function NavbarComponent() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">DreamScape</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">DreamScape</Navbar.Brand>
         <Nav className="ms-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#signup">Sign-up</Nav.Link>
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/signup">Sign-up</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
   );
 }
 
-export default ColorSchemesExample;
+export default NavbarComponent;
