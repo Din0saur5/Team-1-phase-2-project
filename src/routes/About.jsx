fa4d6fa70b194336292987813340d88107c43365
Matthew Tran <matt.h.tran@gmail.com>
2023-11-12 15:39:11 -0500

    css changes

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
     
       <div className="guide-intro">
         <section className="about-section">
-          <h2>Explore DreamScape</h2>
+          <h2><strong>Explore DreamScape</strong></h2>
             <p> In a near-future world, "DreamScape: Beyond Imagination" offers boundless freedom, allowing users to bring their wildest dreams to life in hyper-realistic virtual reality. The DreamScape Generator turns fantasies into immersive realities, with users crafting their worlds, from fantasy realms to sci-fi cities. As players explore their own dreamscapes and unravel the mysteries of a hidden collective realm, they must band together to uncover the truth behind unintended consequences and navigate a web of stories and challenges.</p>
         </section>
       </div>
@@ -57,13 +57,14 @@ const About = () => {
       
 
       <div className="about-info-1">
-        <h3> Endless Freedom </h3>
+        <h3><strong> Endless Freedom </strong></h3>
         <p>
         Our innovative DreamScape Generator empowers users with unparalleled freedom. Unleash your creativity as you transform your wildest fantasies into vivid, interactive experiences. From enchanted fantasy realms to futuristic sci-fi cities, the only limit is the expanse of your imagination.
         </p>
 
-      <h3>
+      <h3><strong>
       Craft Your World
+      </strong>
       </h3>
       <p>
       Take control of your dreamscape. Craft landscapes, design architecture, and populate your virtual world with the characters of your creation. The DreamScape crafting tools provide the means to shape every detail, ensuring that your dreams unfold exactly as you envision them.
@@ -71,8 +72,9 @@ const About = () => {
 
       <p></p>
 
-      <h3>
+      <h3><strong>
       Unravel Collective Realms
+      </strong>
       </h3>
       <p>
       As you delve into your personal dreamscape, discover a hidden collective realm shared by DreamScape enthusiasts. Collaborate with fellow dreamers to uncover the mysteries that lie beneath the surface. The interconnected narratives and challenges will test your imagination and forge bonds with others navigating this fantastical journey.
@@ -104,8 +106,9 @@ const About = () => {
       
       <div className="about-info-2">
 
-      <h3>
+      <h3><strong>
       Unintended Consequences
+      </strong>
       </h3>
       <p>
       Beyond the beauty and marvels of DreamScape, unintended consequences await. Join forces with other players to unveil the truth behind these unforeseen twists. Navigating through a web of stories and challenges, your decisions will shape the destiny of your dreamscape and influence the collective realm.
@@ -114,23 +117,23 @@ const About = () => {
 
       </p>
       
-      <h3>
-      Embark on the Journey:
+      <h3><strong>
+      Embark on the Journey</strong>
       </h3>
       <p>
       DreamScape is not just a virtual reality; it's a journey into the uncharted territories of your mind. Immerse yourself in an experience where creativity knows no bounds, challenges fuel growth, and connections with others transcend the digital realm.
       </p>
 
-      <h3>
-      Dream Together
+      <h3> <strong>
+      Dream Together </strong>
       </h3>
       <p>
       DreamScape beckons dreamers from all walks of life. Whether you're a seasoned explorer or a novice dream-weaver, the collective DreamScape community is a tapestry of diverse stories and visions. Together, we dream, explore, and redefine the very fabric of reality.
       </p>
 
-      <p>
+      <p> <em><strong>
       Join us in DreamScape, where the extraordinary is an everyday occurrence, and the line between dreams and reality is beautifully blurred. Beyond imagination lies a world waiting to be explored—your world.
-      </p>
+      </strong></em></p>
 
     </div>
 
